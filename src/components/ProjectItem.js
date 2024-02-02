0a6340ff0f144c74d50fac6a4a8c4de09a54fff7
Mark Kelsall <markkelsall1995@gmail.com>
2024-02-02 15:02:51 +0300

    final

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -2,6 +2,7 @@ import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
   return (
+    // Main container with class "project-item"
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
