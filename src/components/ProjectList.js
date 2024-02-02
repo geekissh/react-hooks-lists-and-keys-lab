2fa59619cf460211ac3f2fb9ab7ac6f0e3a992c9
Mark Kelsall <markkelsall1995@gmail.com>
2024-02-02 09:11:30 +0300

    final

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,19 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projects.map((project) => (
+          <ProjectItem
+            key={project.id} // Replace with the actual property used as an ID
+            name={project.name} // Replace with the actual property name in your project data
+            about={project.about} // Replace with the actual property name in your project data
+            technologies={project.technologies}
+          />
+        ))}
+      </div>
     </div>
   );
 }
